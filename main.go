0834872f2ca722b1e664fdd2e27a4983559834d6
SabianF <58588133+SabianF@users.noreply.github.com>
2024-11-14 15:23:45 -0800

    feat: added page navigation

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/click-to-edit", getClickToEdit)
 	http.HandleFunc("/click-to-edit/edit", getEdit)
 	http.HandleFunc("/click-to-edit/save", getSave)
-	http.HandleFunc("/click-to-edit/cancel", getSave)
+	http.HandleFunc("/click-to-edit/cancel", getCancel)
 
 	const port = ":3333"
 	fmt.Printf("Starting server on port %s ...\n", port)
@@ -30,6 +31,14 @@ func main() {
 	}
 }
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("GET /\n")
+
+	component := readFile("index.html")
+
+	io.WriteString(w, string(component) + "\n")
+}
+
 func getClickToEdit(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("GET /click-to-edit\n")
 
@@ -41,23 +50,26 @@ func getClickToEdit(w http.ResponseWriter, r *http.Request) {
 func getEdit(w http.ResponseWriter, r *http.Request	) {
 	fmt.Printf("GET /click-to-edit/edit\n")
 
-	component := readFile("components/click-to-edit/enabled.html")
+	io.WriteString(w, readFile("components/click-to-edit/enabled.html"))
+}
 
-	io.WriteString(w, string(component) + "\n")
+func getCancel(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("GET /click-to-edit/save\n")
+
+	io.WriteString(w, readFile("components/click-to-edit/index.html"))
 }
 
 func getSave(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("GET /click-to-edit/save\n")
+	fmt.Printf("Request.body: %#v\n", r.Body)
 
-	component := readFile("components/click-to-edit/index.html")
-
-	io.WriteString(w, string(component) + "\n")
+	io.WriteString(w, readFile("components/click-to-edit/index.html"))
 }
 
-func readFile(filepathRelative string) []byte {
+func readFile(filepathRelative string) string {
 	component, err := os.ReadFile(filepathRelative)
 	if err != nil {
 		panic(err)
 	}
-	return component
+	return string(component)
 }
