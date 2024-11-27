ac0dcb8b3643aef9a90d662f72f91a7868e3ba9f
SabianF <58588133+SabianF@users.noreply.github.com>
2024-11-27 01:53:43 -0800

    fix: favicon

--- modules/common/presentation/components/layout_templ.go
+++ modules/common/presentation/components/layout_templ.go
@@ -301,7 +301,7 @@ func styles() templ.Component {
 			templ_7745c5c3_Var12 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<link rel=\"stylesheet\" href=\"/modules/common/data/sources/assets/pico.min.css\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<link rel=\"stylesheet\" href=\"/modules/common/data/sources/assets/pico.min.css\"><link rel=\"icon\" type=\"image/svg+xml\" href=\"/modules/common/data/sources/assets/favicon.svg\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- modules/common/presentation/components/nav_bar_templ.go
+++ modules/common/presentation/components/nav_bar_templ.go
@@ -30,7 +30,7 @@ func NavBar(title string, items ...templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav><ul><li>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<nav hx-boost=\"true\"><ul><li>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
