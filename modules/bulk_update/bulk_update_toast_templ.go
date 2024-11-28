1442233a5d3d72993d09bd6f5ce79edd007a4e87
SabianF <58588133+SabianF@users.noreply.github.com>
2024-11-27 17:12:24 -0800

    fix: animation of toast

--- modules/bulk_update/bulk_update_toast_templ.go
+++ modules/bulk_update/bulk_update_toast_templ.go
@@ -30,7 +30,7 @@ func bulkUpdateToast(updatedUsers map[string][]string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<span id=\"toast\" aria-live=\"polite\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<article id=\"toast\" aria-live=\"polite\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -111,7 +111,7 @@ func bulkUpdateToast(updatedUsers map[string][]string) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</article>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
