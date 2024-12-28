9319100e53fc9da7e1b90fdfd92d4154a1213fbc
SabianF <58588133+SabianF@users.noreply.github.com>
2024-12-28 01:58:30 -0800

    feat: websocket receive events

--- modules/web_socket/data/repositories/repos.go
+++ modules/web_socket/data/repositories/repos.go
@@ -2,14 +2,21 @@ package web_socket
 
 import (
 	"net/http"
+	"time"
 
 	common_data_repos "github.com/SabianF/htmx-playground/modules/common/data/repositories"
+	pages "github.com/SabianF/htmx-playground/modules/web_socket/presentation/pages"
 
 	gorilla_websocket "github.com/gorilla/websocket"
 )
 
-const ROUTE_WS string = "/ws"
+const ROUTE_PAGE string = "/web-socket"
 
+func getPage(w http.ResponseWriter, r *http.Request) {
+	pages.WebSocketPage().Render(r.Context(), w)
+}
+
+const ROUTE_WS string = "/ws"
 var upgrader = gorilla_websocket.Upgrader{}
 
 func serveWebSocket(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +31,22 @@ func serveWebSocket(w http.ResponseWriter, r *http.Request) {
 
 	common_data_repos.Log("Web socket client connected: %v", ws.RemoteAddr())
 
-	// Listen for events
+	// Listen and handle events
+	sendFrequentMessages(ws)
+	getAndRespondToMessages(ws)
+}
+
+func sendFrequentMessages(ws *gorilla_websocket.Conn) {
+	for {
+		time.Sleep(3 * time.Second)
+		ws.WriteMessage(
+			gorilla_websocket.TextMessage,
+			[]byte("<div hx-swap-oob='beforeend:#chat'><article>Hello, there.</article></div>"),
+		)
+	}
+}
+
+func getAndRespondToMessages(ws *gorilla_websocket.Conn) {
 	for {
 		// Receive message from client
 		msgType, msg, err := ws.ReadMessage()
@@ -35,6 +57,7 @@ func serveWebSocket(w http.ResponseWriter, r *http.Request) {
 		common_data_repos.Log("Web socket message: %v", msg)
 
 		// Send received message back to client
+		time.Sleep(2 * time.Second)
 		err = ws.WriteMessage(msgType, msg)
 		if err != nil {
 			common_data_repos.Log("ws.WriteMessage error: %v\n", err)
@@ -45,4 +68,5 @@ func serveWebSocket(w http.ResponseWriter, r *http.Request) {
 
 func AddRoutes(mux *http.ServeMux) {
 	mux.HandleFunc(ROUTE_WS, serveWebSocket)
+	mux.HandleFunc(ROUTE_PAGE, getPage)
 }
